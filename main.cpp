72188fb4db14d6fe4c13eb006bafbc590c4db7cf
valxntim <72460992+valxntim@users.noreply.github.com>
2022-03-10 11:12:14 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "teste_dominios.h"
+#include "teste_entidades.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    TUUsuario testeUnitario;
+    cout << testeUnitario.run() << endl;
+    return 0;
+}
